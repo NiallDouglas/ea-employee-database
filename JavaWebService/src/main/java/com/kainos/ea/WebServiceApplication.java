8d92eb7377f7e5d90c39fa8f6beb87649c9b5b7b
Matthew Knox <matthew.knox@kainos.com>
2022-09-08 10:32:42 +0100

    Adding WebService Java app

---
+++ JavaWebService/src/main/java/com/kainos/ea/WebServiceApplication.java
@@ -0,0 +1,30 @@
+package com.kainos.ea;
+
+import com.kainos.ea.resources.WebService;
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class WebServiceApplication extends Application<WebServiceConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new WebServiceApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "WebService";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<WebServiceConfiguration> bootstrap) {
+
+    }
+
+    @Override
+    public void run(final WebServiceConfiguration configuration,
+                    final Environment environment) {
+        environment.jersey().register(new WebService());
+    }
+
+}
