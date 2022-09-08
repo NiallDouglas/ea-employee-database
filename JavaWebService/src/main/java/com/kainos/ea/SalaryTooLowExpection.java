2dab27e4d04a4c0553704b1f7c3a12f4ea20b818
Niall Douglas <niall.douglas@kainos.com>
2022-09-08 11:38:31 +0100

    adding employesdb files

---
+++ JavaWebService/src/main/java/com/kainos/ea/SalaryTooLowExpection.java
@@ -0,0 +1,7 @@
+package com.kainos.ea;
+
+public class SalaryTooLowExpection extends IllegalArgumentException{
+    public SalaryTooLowExpection(String s) {
+        super(s);
+    }
+}
---
+++ JavaWebService/src/main/java/com/kainos/ea/resources/BadNumber.java
@@ -0,0 +1,2 @@
+package com.kainos.ea.resources;public class BadNumber {
+}
